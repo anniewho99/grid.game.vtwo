4048dc73e79f5d163930ea6d5fc4b1b579577a16
anniewho99 <xinyuehu1999@gmail.com>
2025-04-23 16:48:46 -0700

    new prolific

--- firebaseconfig.js
+++ firebaseconfig.js
@@ -1,8 +1,9 @@
 const firebaseConfig = {
   apiKey: "AIzaSyDDx_9oVUYHMB0s9uc66c_zEm3mAluWKJE",
   authDomain: "gridgameexptwo.firebaseapp.com",
+  databaseURL: "https://gridgameexptwo-default-rtdb.firebaseio.com",
   projectId: "gridgameexptwo",
-  storageBucket: "gridgameexptwo.appspot.com",
+  storageBucket: "gridgameexptwo.firebasestorage.app",
   messagingSenderId: "236760428129",
   appId: "1:236760428129:web:f2e3b62295de166a134420"
 };
